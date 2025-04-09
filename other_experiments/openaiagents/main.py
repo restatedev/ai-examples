e7050f7120170871fe9c8926b421fa4feefb4db9
Giselle van Dongen <giselle.vd@gmail.com>
2025-04-09 09:56:59 +0200

    Cleanup

--- other_experiments/openaiagents/main.py
+++
@@ -1,8 +0,0 @@
-import restate
-
-from src.openaiagents.services.seat_object import seat_object
-from src.openaiagents.services.booking_object import booking_object
-from src.openaiagents.services.faq_service import faq_service
-from chat_session import chat_service
-
-app = restate.app(services=[chat_service, booking_object, faq_service, seat_object])
