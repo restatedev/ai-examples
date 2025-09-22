7be691386b9586bcd086a399978d91eba68b462d
Giselle van Dongen <giselle@restate.dev>
2025-09-22 15:07:11 +0200

    Add examples git workflows

---
+++ .tools/run_node_tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eufx -o pipefail
+
+SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
+PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
+
+function npm_install_check() {
+    npm install --prefix $1 && npm --prefix $1 run build
+}
+
+npm_install_check $PROJECT_ROOT/typescript/basics
+
+npm_install_check $PROJECT_ROOT/typescript/templates/node
+RESTATE_ENV_ID=env_test RESTATE_API_KEY=key_test npm_install_check $PROJECT_ROOT/typescript/integrations/deployment-lambda-cdk
+npm_install_check $PROJECT_ROOT/typescript/templates/cloudflare-worker
+npm_install_check $PROJECT_ROOT/typescript/templates/nextjs
+
+npm_install_check $PROJECT_ROOT/typescript/templates/typescript-testing
+npm --prefix $PROJECT_ROOT/typescript/templates/typescript-testing run test
+
+npm_install_check $PROJECT_ROOT/typescript/tutorials/tour-of-restate-typescript
+npm_install_check $PROJECT_ROOT/typescript/tutorials/tour-of-orchestration-typescript
+npm_install_check $PROJECT_ROOT/typescript/tutorials/tour-of-workflows-typescript
+
+npm_install_check $PROJECT_ROOT/typescript/patterns-use-cases
+
+npm_install_check $PROJECT_ROOT/typescript/end-to-end-applications/ai-image-workflows
+npm_install_check $PROJECT_ROOT/typescript/end-to-end-applications/food-ordering/app
+npm_install_check $PROJECT_ROOT/typescript/end-to-end-applications/chat-bot
+
+RESTATE_ENV_ID=env_test RESTATE_API_KEY=key_test npm_install_check $PROJECT_ROOT/python/integrations/deployment-lambda-cdk
---
+++ .tools/run_python_tests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eufx -o pipefail
+
+SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
+PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
+
+function python_mypi_lint() {
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip install -r requirements.txt
+  pip install mypy
+  python3 -m mypy .
+  deactivate
+}
+
+pushd $PROJECT_ROOT/python/templates/python && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/basics && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/patterns-use-cases && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/tutorials/tour-of-restate-python && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/tutorials/tour-of-orchestration-python && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/tutorials/tour-of-workflows-python && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/integrations/deployment-lambda-cdk/lib/lambda && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/end-to-end-applications/chat-bot/chatbot && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/end-to-end-applications/rag-ingestion && python_mypi_lint && popd
+pushd $PROJECT_ROOT/python/end-to-end-applications/food-ordering/app && python_mypi_lint && popd
