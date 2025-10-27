e1d1e3c8a3b00804c53e1fbf514bab7b61b9cfff
Giselle van Dongen <giselle@restate.dev>
2025-10-27 12:15:38 +0100

    wip: add agent and quiz agent

--- google-adk/example/__main__.py
+++ google-adk/example/__main__.py
@@ -3,14 +3,16 @@ import asyncio
 import restate
 
 from app.agent import agent_service
+from app.quiz_agent import quiz_agent_service
 
-app = restate.app(services=[agent_service])
+
+app = restate.app(services=[agent_service, quiz_agent_service])
 
 
 def main():
     """Entry point for running the app."""
     conf = hypercorn.Config()
-    conf.bind = ["0.0.0.0:9080"]
+    conf.bind = ["0.0.0.0:9081"]
     asyncio.run(hypercorn.asyncio.serve(app, conf))
 
 
--- google-adk/example/app/agent.py
+++ google-adk/example/app/agent.py
@@ -1,4 +1,5 @@
 import restate
+from datetime import datetime
 from google.adk.agents.llm_agent import Agent
 from google.adk.tools.tool_context import ToolContext
 from google.adk.runners import Runner
@@ -10,13 +11,38 @@ from app.utils.restate_session_service import RestateSessionService
 
 APP_NAME = "agent_app"
 
+async def get_weather(tool_context: ToolContext, city: str) -> dict:
+    """Retrieves the current weather report for a specified city."""
+    restate_context: restate.ObjectContext = tool_context.session.state["restate_context"]
+
+    def get_weather():
+        if city.lower() == "new york":
+            return {
+                "status": "success",
+                "report": (
+                    "The weather in New York is sunny with a temperature of 25 degrees"
+                    " Celsius (77 degrees Fahrenheit)."
+                ),
+            }
+        else:
+            return {
+                "status": "error",
+                "error_message": f"Weather information for '{city}' is not available.",
+            }
+
+    return await restate_context.run_typed("get weather", lambda: get_weather())
+
+
 async def get_current_time(tool_context: ToolContext, city: str) -> dict:
     """Returns the current time in a specified city."""
-    restate_context = tool_context.session.state["restate_context"]
+    restate_context: restate.ObjectContext = tool_context.session.state["restate_context"]
 
     # session needs to be Restate
-    time_float = await restate_context.time()
-    time_string = str(time_float)
+    sec = await restate_context.time()
+    dt_object = datetime.fromtimestamp(sec)
+
+    # Convert to readable string
+    time_string = dt_object.strftime("%Y-%m-%d %H:%M:%S")
     return {"status": "success", "city": city, "time": time_string}
 
 class Prompt(BaseModel):
@@ -36,10 +62,10 @@ async def run(ctx: restate.ObjectContext, prompt: Prompt) -> str:
 
     agent = Agent(
         model=durable_model_calls(ctx, 'gemini-2.5-flash'),
-        name='time_agent',
-        description="Tells the current time in a specified city.",
-        instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
-        tools=[get_current_time],
+        name='weather_time_agent',
+        description="Agent to answer questions about the time and weather in a city.",
+        instruction="You are a helpful agent who can answer user questions about the time and weather in a city.",
+        tools=[get_weather, get_current_time],
     )
 
     runner = Runner(agent=agent, app_name=APP_NAME, session_service=session_service)
