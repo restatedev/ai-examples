cd4fba68d861a10e23a76d53b8e5da7f804850d2
Giselle van Dongen <giselle@restate.dev>
2025-11-20 11:11:02 +0100

    improve commenting of basic example

--- google-adk/example/app/durable_agent.py
+++ google-adk/example/app/durable_agent.py
@@ -19,35 +19,33 @@ APP_NAME = "agents"
 async def get_weather(tool_context: ToolContext, city: str) -> WeatherResponse:
     """Get the current weather for a given city."""
     restate_context = tool_context.session.state["restate_context"]
-
-    #  call tool wrapped as Restate durable step
+    #  Do one or more durable steps using the Restate context
     return await restate_context.run_typed(
         f"Get weather {city}", call_weather_api, city=city
     )
 
 
-# AGENT
+# Specify your agent in the default ADK way
 agent = Agent(
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash",
     name="weather_agent",
     description="Agent that provides weather updates for cities.",
-    instruction="You are a helpful agent that provides weather updates. Use the get_weather tool to fetch current weather information.",
+    instruction="You are a helpful agent that provides weather updates. "
+                "Use the get_weather tool to fetch current weather information.",
     tools=[get_weather],
 )
 
-
-# HANDLER
 agent_service = restate.Service("WeatherAgent")
 
 @agent_service.handler()
 async def run(ctx: restate.Context, req: WeatherPrompt) -> str:
     session_id = str(ctx.uuid())
-
     session_service = InMemorySessionService()
     await session_service.create_session(
         app_name=APP_NAME, user_id=req.user_id, session_id=session_id
     )
 
+    # Use the Restate plugin to enable retries, recovery, and workflows
     app = App(name=APP_NAME, root_agent=agent, plugins=[RestatePlugin(ctx)])
     runner = Runner(app=app, session_service=session_service)
 
@@ -61,5 +59,4 @@ async def run(ctx: restate.Context, req: WeatherPrompt) -> str:
         async for event in events:
             if event.is_final_response() and event.content and event.content.parts:
                 final_response = event.content.parts[0].text
-
     return final_response
