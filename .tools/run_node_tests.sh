04b3fab1ca377fca8f7aa56e732eafe3135ff110
Giselle van Dongen <giselle.vd@gmail.com>
2026-04-24 10:35:03 +0200

    Add TS examples to release flow (#53)
    * Add TS examples to release flow
    
    * Fix CI

--- .tools/run_node_tests.sh
+++ .tools/run_node_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eufx -o pipefail
+set -eux -o pipefail
 
 SELF_PATH=${BASH_SOURCE[0]:-"$(command -v -- "$0")"}
 PROJECT_ROOT="$(dirname "$SELF_PATH")/.."
@@ -18,3 +18,6 @@ npm_install_check $PROJECT_ROOT/vercel-ai/tour-of-agents
 # Restate-only examples
 npm_install_check $PROJECT_ROOT/typescript-restate-only/template
 npm_install_check $PROJECT_ROOT/typescript-restate-only/tour-of-agents
+for example_dir in $PROJECT_ROOT/typescript-restate-only/examples/*/; do
+    npm_install_check "$example_dir"
+done
